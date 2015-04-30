26ef315fb2d212666e77c5006ad822c40256a4d6
defrost256 <danko.barna@gmail.com>
2015-04-30 22:19:52 +0200

    MemoryNode base

---
+++ GEN-NET/MemoryNode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GEN_NET
+{
+	public class MemoryNode<T> : NeuralNode<T>
+	{
+		Queue<T> memoryQueue;
+		public int memoryDepth;
+
+		public MemoryNode(int memoryDepth)
+			: base()
+		{
+			memoryQueue = new Queue<T>();
+			this.memoryDepth = memoryDepth;
+		}
+
+		public override void calculateOutput()
+		{
+			base.calculateOutput();
+			memoryQueue.Enqueue(Output);
+
+		}
+	}
+}
